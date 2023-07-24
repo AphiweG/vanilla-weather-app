5293dfd6db22c9409c7c9919bdc5cec47fa2da7c
AphiweG <49280947+AphiweG@users.noreply.github.com>
2023-07-24 10:09:28 +0200

    I added the relevent weather icon from the API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
+const currentDate = new Date();
+
+const date = "currentDate";
+const dateElement = document.getElementById(date);
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const dayOfWeek = daysOfWeek[currentDate.getDay()];
+const hours = String(currentDate.getHours()).padStart(2, "0");
+const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+const currentTime = `${dayOfWeek} ${hours}:${minutes}`;
+console.log(currentTime);
+dateElement.textContent = `${currentTime}`;
+
 function displayTemperature(response) {
-  console.log(response.data.main);
+  console.log(response.data);
   const temeratureElement = document.getElementById("temperature");
   const cityElement = document.getElementById("city");
-  const descriptionElement = document.getElementById("description");
+  const descriptionElement = document.getElementById("desciption");
   const humidityElement = document.getElementById("humidity");
   const windElement = document.getElementById("wind");
+  const iconElement = document.getElementById("icon");
   temeratureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 const apiKey = "9ab9c8883313d4e254caf910926ea7c5";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Midrand&appid=${apiKey}&units=metric`;
+const city = "Midrand";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
